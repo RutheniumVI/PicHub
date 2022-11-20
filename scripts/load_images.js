fdbcbf6aa1dd44eedd156d9a213a8e0a6947021a
fahmed8383 <fahmed4030@gmail.com>
2022-11-20 10:00:16 -0500

    added albums and fixed issues with photos page

--- scripts/load_images.js
+++ scripts/load_images.js
@@ -3,85 +3,92 @@ images = [
         src: "../files/ocean.jpg",
         dateTaken: new Date("2022-01-22"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["water", "mountains"]
+        tags: ["water", "mountains"],
+        albums: ["favs", "nature"]
     },
     {
         src: "../files/mountains.jpg",
         dateTaken: new Date("2022-01-21"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["mountains"]
+        tags: ["mountains"],
+        albums: ["favs", "nature"]
     },
     {
         src: "../files/tree.jpg",
         dateTaken: new Date("2022-01-22"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature"]
     },
     {
         src: "../files/dog.jpg",
         dateTaken: new Date("2022-01-25"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["dog"]
+        tags: ["dog"],
+        albums: ["favs", "dogs"]
     },
     {
-        src: "../files/tree.jpg",
+        src: "../files/tree_2.jpeg",
         dateTaken: new Date("2022-01-25"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature"]
     },
     {
-        src: "../files/dog.jpg",
+        src: "../files/dog_2.jpg",
         dateTaken: new Date("2022-01-25"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["dog"]
+        tags: ["dog"],
+        albums: ["favs", "dogs"]
     },
     {
-        src: "../files/tree.jpg",
+        src: "../files/tree_3.jpeg",
         dateTaken: new Date("2022-01-25"),
         dateUploaded: new Date("2022-01-26"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature", "favs"]
     },
     {
-        src: "../files/ocean.jpg",
+        src: "../files/ocean_2.jpg",
         dateTaken: new Date("2022-02-25"),
         dateUploaded: new Date("2022-02-26"),
-        tags: ["water", "mountains"]
+        tags: ["water"],
+        albums: ["nature"]
     },
     {
-        src: "../files/mountains.jpg",
+        src: "../files/mountains_2.jpeg",
         dateTaken: new Date("2022-02-25"),
         dateUploaded: new Date("2022-02-26"),
-        tags: ["mountains"]
+        tags: ["mountains"],
+        albums: ["nature"]
     },
     {
-        src: "../files/tree.jpg",
+        src: "../files/tree_4.jpeg",
         dateTaken: new Date("2022-02-26"),
         dateUploaded: new Date("2022-02-27"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature"]
     },
     {
-        src: "../files/dog.jpg",
+        src: "../files/dog_3.jpg",
         dateTaken: new Date("2022-02-26"),
         dateUploaded: new Date("2022-02-27"),
-        tags: ["dog"]
+        tags: ["dog"],
+        albums: ["dogs"]
     },
     {
-        src: "../files/tree.jpg",
+        src: "../files/tree_5.jpeg",
         dateTaken: new Date("2022-02-26"),
         dateUploaded: new Date("2022-02-27"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature", "favs"]
     },
     {
-        src: "../files/dog.jpg",
+        src: "../files/tree_6.jpeg",
         dateTaken: new Date("2022-02-26"),
         dateUploaded: new Date("2022-02-27"),
-        tags: ["dog"]
-    },
-    {
-        src: "../files/tree.jpg",
-        dateTaken: new Date("2022-02-26"),
-        dateUploaded: new Date("2022-02-27"),
-        tags: ["tree"]
+        tags: ["tree"],
+        albums: ["nature"]
     },
 ]
 
